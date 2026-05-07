26a07edb7fd96cee24e4d07904c264d3c51b43ce
anapmagalhaess <bruclaramagalhaes2015@gmail.com>
2026-05-06 23:48:12 -0300

    feat: Pop-up que sinaliza erro e animação ao aparecer.

--- js/login.js
+++ js/login.js
@@ -1,24 +1,22 @@
-const modal = document.getElementById('popup-modal')
-
-function closeModal() {
-    modal.style.display = "none";
-}
-
 //Altera qualquer texto do HTML
 function alterarTexto(id, texto) {
-    let objeto = document.getElementById(id) //Resgata o campo para alterar
+    let objeto = document.getElementById(id); //Resgata o campo para alterar
     if (objeto) { //verifica se o elemento existe
-        objeto.innerHTML = texto //altera o campo com texto novo
+        objeto.innerHTML = texto; //altera o campo com texto novo
     }
 }
 
-const form = document.getElementById('form-login')
+const form = document.getElementById('form-login');
+const modal = document.getElementById('popup-modal');
+
 form.addEventListener('submit', (event) => {  //Ao dar enter no botão, irá executar a ação.
     event.preventDefault();
 
     //Resgatando resposta do usuário pelo ID do input no HTML e atribuindo a variável
-    const dataNasc = document.getElementById('birthday').value; //.value Irá resgatar somente valor escrito pelo usuario
-    const nome = document.getElementById('nome').value;
+    const dataNasc = document.getElementById('birthday').value //.value Irá resgatar somente valor escrito pelo usuario
+    const nome = document.getElementById('nome').value
+    const senha = document.getElementById('password').value
+    const email = document.getElementById('email').value
     
     //Cálculo da idade com base no mes atual < mes de aniversario
     const hoje = new Date(); //Objeto com data exata de agora
@@ -32,16 +30,22 @@ form.addEventListener('submit', (event) => {  //Ao dar enter no botão, irá exe
         idade--;                                                        // mes < 0 = aniversario ainda nao chegou, logo decrementa a idade
     }
 
+    //validação de campo obrigatorio vazio
+    if (nome.trim() == '' || dataNasc == '' || senha.trim() == '' || email.trim() == '' ) {
+        modal.classList.add('modal--active');
+        return;
+    } 
     //Validação de idade (-14 não podem acessar o app)
+
     if (idade < 14) {
         alterarTexto('modal-title', "Idade insuficiente");
-        alterarTexto('modal-message', "Idade inserida inferior a permitida para acessar o aplicativo.")
-        modal.style.display = 'flex'; //altera estado do display (sempre usar aspas simples ao puxar um item fora do JS)
+        alterarTexto('modal-message', "Idade inserida inferior a permitida para acessar o aplicativo.");
+        modal.classList.add('modal--active'); //altera visibilidade
         return;
     }
 
     //Caso tudo estiver correto
-    sessionStorage.setItem('usuarioLogado', 'true') //sessionStore vem do WebStorage API, ira armazenar esses dados por sessao
+    sessionStorage.setItem('usuarioLogado', 'true'); //sessionStore vem do WebStorage API, ira armazenar esses dados por sessao (reset no F5 ou fechamento)
     sessionStorage.setItem('nomeUsuario', nome.split(' ')[0]); //Puxa o nome inserido no input, separa cada palavra por espaço, coloca em um array 
                                                                 // e resgata somente o Nome, ou seja, item inicial da lista (sempre começa por [0])
     window.location.href = "../index.html"; //Leva o usuário para página inicial
---
+++ js/modal.js
@@ -0,0 +1,4 @@
+function closeModal() { //fechar modal
+    const modal = document.getElementById('popup-modal');
+    modal.classList.remove('modal--active');
+}
